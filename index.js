3a12f6f9c012c8c810ad9ef30bc489e2ef6da2cd
cemekaagbafoundation <c.emekaagbafoundation@gmail.com>
2026-03-28 04:07:55 +0000

    Fix index.js all routes

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/auth', require('./routes/auth'));
+app.use('/api/auth',         require('./routes/auth'));
 app.use('/api/apply',        require('./routes/apply'));
 app.use('/api/newsletter',   require('./routes/newsletter'));
 app.use('/api/donations',    require('./routes/donations'));
@@ -15,6 +15,7 @@ app.use('/api/content',      require('./routes/content'));
 app.use('/api/programs',     require('./routes/programs'));
 app.use('/api/contact',      require('./routes/contact'));
 app.use('/api/transparency', require('./routes/transparency'));
+app.use('/api/partners',     require('./routes/partners'));
 
 app.get('/', (req, res) => res.send('Agba Foundation API is running'));
 
