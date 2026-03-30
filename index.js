ecfa6583ba897b21f2843f39de5c15362c8414dd
cemekaagbafoundation <c.emekaagbafoundation@gmail.com>
2026-03-30 05:50:51 +0000

    Add hero-images route, fix all routes in index.js

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-app.use(cors());
+
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'x-admin-token', 'Authorization']
+}));
+
 app.use(express.json());
 
 app.use('/api/auth',         require('./routes/auth'));
@@ -15,8 +21,8 @@ app.use('/api/content',      require('./routes/content'));
 app.use('/api/programs',     require('./routes/programs'));
 app.use('/api/contact',      require('./routes/contact'));
 app.use('/api/transparency', require('./routes/transparency'));
-app.use('/api/hero-images', require('./routes/heroImages'));
 app.use('/api/partners',     require('./routes/partners'));
+app.use('/api/hero-images',  require('./routes/heroImages'));
 
 app.get('/', (req, res) => res.send('Agba Foundation API is running'));
 
